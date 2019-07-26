ea8d496fd8a2528f982c4428f1c41225d788ac24
TheDiamondDoge <supportonly@yandex.ru>
2019-07-26 11:19:15 +0300

    info tab -> edit button

--- pmboard-gui-bp3/src/components/pages/pws/tabs/infoTab/infoTab.js
+++ pmboard-gui-bp3/src/components/pages/pws/tabs/infoTab/infoTab.js
@@ -4,12 +4,26 @@ import MilestoneTable from "../../../../milestoneTable/milestoneTable";
 import {INFO_MAIN_INFO, INFO_URLS} from "./infoTabObject";
 import {FieldName} from "../../../../fieldName/fieldName";
 import {FieldValue} from "../../../../fieldValue/fieldValue";
+import {Button, Intent} from "@blueprintjs/core";
 import styles from './infoTab.module.css'
 
 export default class InfoTab extends React.Component {
+    state = {
+        isLoading: false
+    };
+
     render() {
         return (
             <div>
+                <Button
+                    minimal={true}
+                    text={"Edit"}
+                    icon={"edit"}
+                    large={true}
+                    loading={this.state.isLoading}
+                    onClick={() => (this.setState((prevState) => ({isLoading: !prevState.isLoading})))}
+                    intent={Intent.PRIMARY}
+                />
                 <CustomCard>
                     {
                         INFO_MAIN_INFO.map((obj, key) => (
