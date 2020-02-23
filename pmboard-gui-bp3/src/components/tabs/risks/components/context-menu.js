58e551520ed86941d670e97318e9b81dac7d362b
TheDiamondDoge <supportonly@yandex.ru>
2020-02-23 19:38:08 +0300

    risks tab refactor/fixes

--- pmboard-gui-bp3/src/components/tabs/risks/components/context-menu.js
+++ pmboard-gui-bp3/src/components/tabs/risks/components/context-menu.js
@@ -2,16 +2,18 @@ import React from 'react';
 import {Menu, MenuItem} from "@blueprintjs/core";
 import PropTypes from 'prop-types';
 
-export default function ContextMenu(props) {
-    const {onEdit, ...menuProps} = props;
-    return (
-        <Menu {...menuProps}>
-            <MenuItem icon={"edit"}
-                      text="Edit 'Reported' flag"
-                      onClick={onEdit}
-            />
-        </Menu>
-    )
+export default class ContextMenu extends React.PureComponent {
+    render() {
+        const {onEdit, ...menuProps} = this.props;
+        return (
+            <Menu {...menuProps}>
+                <MenuItem icon={"edit"}
+                          text="Edit 'Reported' flag"
+                          onClick={onEdit}
+                />
+            </Menu>
+        )
+    }
 }
 
 ContextMenu.propTypes = {
--- pmboard-gui-bp3/src/components/tabs/risks/components/table-footer.js
+++ pmboard-gui-bp3/src/components/tabs/risks/components/table-footer.js
@@ -2,24 +2,26 @@ import React from 'react';
 import {Button, Intent} from "@blueprintjs/core";
 import PropTypes from 'prop-types';
 
-export default function TableFooter(props) {
-    const {onExcelExport, onExcelImport} = props;
-    return (
-        <>
-            <Button text={"Export to Excel"}
-                    icon={"export"}
-                    onClick={onExcelExport}
-                    intent={Intent.PRIMARY}
-                    minimal
-            />
-            <Button text={"Import Excel"}
-                    icon={"import"}
-                    onClick={onExcelImport}
-                    intent={Intent.PRIMARY}
-                    minimal
-            />
-        </>
-    )
+export default class TableFooter extends React.PureComponent {
+    render() {
+        const {onExcelExport, onExcelImport} = this.props;
+        return (
+            <>
+                <Button text={"Export to Excel"}
+                        icon={"export"}
+                        onClick={onExcelExport}
+                        intent={Intent.PRIMARY}
+                        minimal
+                />
+                <Button text={"Import Excel"}
+                        icon={"import"}
+                        onClick={onExcelImport}
+                        intent={Intent.PRIMARY}
+                        minimal
+                />
+            </>
+        )
+    }
 }
 
 TableFooter.propTypes = {
